f69a78d8b20c01a0322f9ff6e4b2803585088ad7
minyitan <1913343670@qq.com>
2020-10-14 09:42:08 +0800

    I created four php wed pages(create、overview\view\news)

---
+++ app/Controllers/News.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace App\Controllers;
+
+use App\Models\NewsModel;
+use CodeIgniter\Controller;
+
+/**
+ * Description of News
+ *
+ * @author Admin
+ */
+class News extends Controller {
+
+    //put your code here
+    public function index() {
+       
+        $model = new NewsModel();
+        
+        $data = [
+            'news' => $model->getNews(),
+            'title' => 'News archive',
+        ];
+ 
+        echo view('templates/header', $data);
+        echo view('news/overview', $data);
+        echo view('templates/footer', $data);
+        //echo "Controller";
+    }
+
+    public function view($slug = null) {
+        $model = new NewsModel();
+
+        $data['news'] = $model->getNews($slug);
+
+        if (empty($data['news'])) {
+            throw new \CodeIgniter\Exceptions\PageNotFoundException('Cannot find the news item: ' . $slug);
+        }
+
+        $data['title'] = $data['news']['title'];
+
+        echo view('templates/header', $data);
+        echo view('news/view', $data);
+        echo view('templates/footer', $data);
+    }
+
+}
